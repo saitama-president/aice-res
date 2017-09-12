717ab66027b1aeac6f7bf372c483aaf89cb64ca2
s-yoshihara <yoshihara@saitama-tec.co.jp>
2017-09-12 15:36:08 +0900

    admin setting added

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,13 @@ Route::get('map',function(){
     return view("map");
 });
 
+/*管理者メニュ-*/
+Route::group(['middleware' => ['auth',"admin"]], function () {
+    Route::get('admin',function(){
+        return view("admin");
+    });
+}
+
 $messages = [
     "ん？　もう一回",
     "<a href='http://chiebukuro.yahoo.co.jp/search/?p=｛Q｝'>Yahoo知恵袋でやれクソ雑魚ナメクジ</a>",
